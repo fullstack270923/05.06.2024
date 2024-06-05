a42dbe2abb732b69ad4cea7e29e357595bdc358c
fullstack290623 <fullstack290623@gmail.com>
2024-06-05 21:07:28 +0300

    .

---
+++ proj1/src/com/company/Car.java
@@ -0,0 +1,79 @@
+package com.company;
+
+// Lombok
+
+public class Car {
+  // function create_car(_type, _model, _color, speed=0)
+  //  {
+  //     return { type: _type, model: _model,
+  //                color: _color, speed: _speed } }
+  //
+  // const car =  { type: "honda", model: "civic",
+  //                color: "red", speed: 100 }
+  //
+    // data -- attributes -- properties
+    String type;
+    String model;
+    String color;
+    int speed;
+    int doors;
+
+    static int max_speed = 140;
+
+    // functions
+  // CTOR constructor
+
+  // default
+  //public Car() {
+  //}
+
+//  public Car() {
+//    System.out.println("new car created...");
+//  }
+
+  public Car(String type, String model, String color, int speed, int doors) {
+    this.type = type;
+    this.model = model;
+    this.color = color;
+    this.speed = speed;
+    this.doors = doors;
+  }
+
+  public Car(String type, String model, String color, int doors) {
+    this(type, model, color, 0, doors); // calls ctor
+    boolean is200legal = isSpeedLegal(200);
+    //this.type = type;
+    //this.model = model;
+    //this.color = color;
+    //this.doors = doors;
+    //this.speed = 0;
+  }
+
+  public boolean isFamilyCar() {
+    if (this.doors >= 4)
+      return true;
+    return false;
+    //return this.doors >= 4;
+  }
+
+  public static boolean isSpeedLegal(int _speed) {
+    // boolean leagl = isthisCarsSpeedLegal(); // ERROR
+    return _speed <= max_speed;
+  }
+
+  public boolean isthisCarsSpeedLegal() {
+    boolean is200legal = isSpeedLegal(200);
+    return this.speed <= max_speed;
+  }
+
+  @Override
+  public String toString() {
+    return "Car{" +
+            "type='" + this.type + '\'' +
+            ", model='" + this.model + '\'' +
+            ", color='" + this.color + '\'' +
+            ", speed=" + this.speed +
+            ", doors=" + this.doors +
+            '}';
+  }
+}
--- proj1/src/com/company/Main.java
+++ proj1/src/com/company/Main.java
@@ -2,10 +2,42 @@ package com.company;
 
 public class Main {
 
+    // Main m = new Main();
+    // m.main();
 
+    // Main.main();
 
     public static void main(String[] args) {
-	    // write your code here
+
+        new Car("honda", "civic", "Red", 5); // no one saves the car
+        int x = 1;
+        String name = "danny";
+        Car honda = new Car("honda", "civic", "Red", 120, 5);
+        honda.speed = 100;
+        honda.doors = 5;
+        honda.type = "Honda";
+
+        // new --> constructor
+        Car mitsubishi = new Car("Mitsubishi", "Outlander", "Black", 180, 7);
+        mitsubishi.type = "Mitsubishi";
+        mitsubishi.model = "Outlander";
+        mitsubishi.speed = 220;
+
+        System.out.println(honda);
+
+        System.out.println("max speed = " + Car.max_speed);
+        Car.max_speed = 180;
+
+        System.out.println(mitsubishi.isFamilyCar());
+
+        System.out.println("220 is legal? "+ Car.isSpeedLegal(220));
+
+        System.out.println("mitsubishi legal? "+ mitsubishi.isthisCarsSpeedLegal());
+
+
+        //System.out.println("max speed = " + Car.speed); // Error
+        // <Car type="honda" />
+        // <Car type="mitsubishi" />
     }
 
 
